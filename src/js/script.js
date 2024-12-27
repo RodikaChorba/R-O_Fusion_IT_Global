3a2dea7e0f6107b8d4c58bcae3dcbdd9624c4770
Rodika Chorba <158561902+RodikaChorba@users.noreply.github.com>
2024-12-27 10:03:21 +0100

    site skeleton

---
+++ src/js/script.js
@@ -0,0 +1,4 @@
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+  alert("Дякуємо за ваше повідомлення! Ми зв’яжемося з вами найближчим часом.");
+});
